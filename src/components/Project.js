97b65d3120d7e239edac67eea5560117f77edba0
Ghazaeh-J <ghazal.javadi@gmail.com>
2022-08-22 19:26:35 -0700

    Fixed demo/code link

--- src/components/Project.js
+++ src/components/Project.js
@@ -54,12 +54,12 @@ export default function Project() {
                </div>
                <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
                {
-                projects.map(({id, src}) => (
+                projects.map(({id, src, demoLink, codeLink}) => (
                     <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
                     <img src={src} alt="" className="rounded-md duration-200 hover:scale-105"/>
                     <div className="flex items-center justify-center">
-                        <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-150">Demo</button>
-                        <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-150">Code</button>
+                        <a className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-150" href={demoLink} target="_blank" rel="noreferrer">Demo</a>
+                        <a className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-150" href={codeLink} target="_blank" rel="noreferrer">Code</a>
                     </div>
                 </div>       
                 ))
